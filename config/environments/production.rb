a2dbf5a80e70c03008e7d61760f5835a71c6edac
bpis <devendrab@blueplanetinfosolutions.com>
2012-11-23 21:51:56 +0530

    changes config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ StartBarter::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'http://starterbarter.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'starterbarter.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
