c2d1934cb76afed91f85d67bc746424af43b723f
bpis <devendrab@blueplanetinfosolutions.com>
2012-12-04 20:44:17 +0530

    displayed skill proficiency

--- app/models/user.rb
+++ app/models/user.rb
@@ -102,7 +102,6 @@ class User < ActiveRecord::Base
   # For Linkedin Authentication with omniauth - Linkedin
   def self.find_for_linkedin_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    debugger
     unless user    
         dob = []
         if auth.extra.raw_info.dateOfBirth.present?
