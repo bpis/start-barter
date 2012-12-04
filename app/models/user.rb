cc89ee926a8c25f7f548ca6ac979d7ff61742c30
bpis <devendrab@blueplanetinfosolutions.com>
2012-12-04 17:50:31 +0530

    added group model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,13 @@ class User < ActiveRecord::Base
   after_create :create_user_skills, :unless => :check_provider?
   after_create :create_user_experiences, :unless => :check_provider?
   after_create :create_user_educations, :unless => :check_provider?
+  after_create :create_user_groups, :unless => :check_provider?
   # Associations
   has_one :profile
   has_many :skills
   has_many :experiences
   has_many :educations
+  has_many :groups
   belongs_to :country
   
   #Image Uploader
@@ -31,6 +33,7 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :skills
   accepts_nested_attributes_for :experiences
   accepts_nested_attributes_for :educations
+  accepts_nested_attributes_for :groups
   
 
   #validations
@@ -78,12 +81,12 @@ class User < ActiveRecord::Base
                       email:auth.info.email,
                       password:Devise.friendly_token[0,20],
                       about_me:auth.extra.raw_info.bio.present? ? auth.extra.raw_info.bio : "",
-                         dob:auth.extra.raw_info.birthday.present? ? auth.extra.raw_info.birthday : "",
+                      dob:auth.extra.raw_info.birthday.present? ? auth.extra.raw_info.birthday : "",
                       hometown:auth.extra.raw_info.hometown.present? && auth.extra.raw_info.hometown.name.present? ? auth.extra.raw_info.hometown.name : "",
                       location:auth.extra.raw_info.location.present? && auth.extra.raw_info.location.name.present? ? auth.extra.raw_info.location.name : "",
                       relationships:auth.extra.raw_info.relationship_status.present? ? auth.extra.raw_info.relationship_status : "",
                       gender:auth.extra.raw_info.gender.present? ? auth.extra.raw_info.gender : "",
-                         organisation:auth.extra.raw_info.work.present? && auth.extra.raw_info.work[0].employer.present? ?  auth.extra.raw_info.work[0].employer.name : "",
+                      organisation:auth.extra.raw_info.work.present? && auth.extra.raw_info.work[0].employer.present? ?  auth.extra.raw_info.work[0].employer.name : "",
                       designation:auth.extra.raw_info.work.present? && auth.extra.raw_info.work[0].position.present? ? auth.extra.raw_info.work[0].position.name : "",
                       facebook_url:auth.info.urls.Facebook.present? ? auth.info.urls.Facebook : "" ,
                       educational_details:auth.extra.raw_info.education.present? ? auth.extra.raw_info.education[1].school.name : "" ,
@@ -108,67 +111,71 @@ class User < ActiveRecord::Base
           dob << auth.extra.raw_info.dateOfBirth.year
         end        
         user = User.new(username:auth.info.name.present? ? auth.info.name : "",
-                      first_name:auth.extra.raw_info.firstName.present? ? auth.extra.raw_info.firstName : "",
-                      last_name:auth.extra.raw_info.lastName.present? ? auth.extra.raw_info.lastName : "",
-                      provider:auth.provider.present? ? auth.provider :  "",
-                      uid: auth.uid.present? ? auth.uid : "",
-                      email: auth.extra.raw_info.emailAddress.present? ? auth.extra.raw_info.emailAddress : "",
-                      password:Devise.friendly_token[0,20],
-                      company_name:auth.extra.raw_info.industry.present? ? auth.extra.raw_info.industry : "",
-                      hometown:auth.extra.raw_info.location.name.present? ? auth.extra.raw_info.location.name : "",
-                      location:auth.extra.raw_info.location.name.present? ? auth.extra.raw_info.location.name : "",                      
-                      dob:dob.join("-"),            
-                      organisation:auth.extra.raw_info.industry.present? ? auth.extra.raw_info.industry : "",   
-                      designation:auth.extra.raw_info.headline.present? ? auth.extra.raw_info.headline : "",
-                      facebook_url:auth.extra.raw_info.publicProfileUrl.present? ? auth.extra.raw_info.publicProfileUrl : "",
-                      profile_picture:auth.extra.raw_info.pictureUrl.present? ? auth.extra.raw_info.pictureUrl : "",  
-                      facebook_image:auth.extra.raw_info.pictureUrl.present? ? auth.extra.raw_info.pictureUrl : "", 
+                        first_name:auth.extra.raw_info.firstName.present? ? auth.extra.raw_info.firstName : "",
+                        last_name:auth.extra.raw_info.lastName.present? ? auth.extra.raw_info.lastName : "",
+                        provider:auth.provider.present? ? auth.provider :  "",
+                        uid: auth.uid.present? ? auth.uid : "",
+                        email: auth.extra.raw_info.emailAddress.present? ? auth.extra.raw_info.emailAddress : "",
+                        password:Devise.friendly_token[0,20],
+                        company_name:auth.extra.raw_info.industry.present? ? auth.extra.raw_info.industry : "",
+                        hometown:auth.extra.raw_info.location.name.present? ? auth.extra.raw_info.location.name : "",
+                        location:auth.extra.raw_info.location.name.present? ? auth.extra.raw_info.location.name : "",                      
+                        dob:dob.join("-"),            
+                        organisation:auth.extra.raw_info.industry.present? ? auth.extra.raw_info.industry : "",   
+                        designation:auth.extra.raw_info.headline.present? ? auth.extra.raw_info.headline : "",
+                        facebook_url:auth.extra.raw_info.publicProfileUrl.present? ? auth.extra.raw_info.publicProfileUrl : "",
+                        profile_picture:auth.extra.raw_info.pictureUrl.present? ? auth.extra.raw_info.pictureUrl : "",  
+                        facebook_image:auth.extra.raw_info.pictureUrl.present? ? auth.extra.raw_info.pictureUrl : "", 
                       )
                              
         user.skip_confirmation!
         user.save!
         user_profile =  user.profile.update_attributes(
-                                  company_name:auth.extra.raw_info.industry.present? ? auth.extra.raw_info.industry : "",
+                              company_name:auth.extra.raw_info.industry.present? ? auth.extra.raw_info.industry : "",
                               tagline:auth.extra.raw_info.headline.present? ? auth.extra.raw_info.headline : "",
                               overview:auth.extra.raw_info.summary.present? ? auth.extra.raw_info.summary : "",
                               phone_no:auth.extra.raw_info.phoneNumbers.values[1].first.phoneNumber.present? ? auth.extra.raw_info.phoneNumbers.values[1].first.phoneNumber : "",
                               address_line_1:auth.extra.raw_info.mainAddress.present? ? auth.extra.raw_info.mainAddress : "",
-                              address_line_2:auth.extra.raw_info.mainAddress.present? ? auth.extra.raw_info.mainAddress : "")
-       
-       
+                              address_line_2:auth.extra.raw_info.mainAddress.present? ? auth.extra.raw_info.mainAddress : ""
+                              )
+        
         user_skills = auth.extra.raw_info.skills.values[1]
         if auth.extra.raw_info.skills.values[1].present?
           user_skills.each do |s|
             u = user.skills.build(name:s.skill.name, user_id:user.id)
           end
         end
-       
         
         user_experiences = auth.extra.raw_info.positions.values[1]
-        if auth.extra.raw_info.positions.values[1].present?
-          
-         user_experiences.each do |e|
-          sd = []
-          ed = []
-          if e.startDate.present?
-            sd << e.startDate.month
-            sd << e.startDate.year
-            end
-          
-          if e.endDate.present?
-            ed << e.endDate.month 
-            ed << e.endDate.year
+        if auth.extra.raw_info.positions.values[1].present?   
+          user_experiences.each do |e|
+            sd = []
+            ed = []
+            if e.startDate.present?
+              sd << e.startDate.month
+              sd << e.startDate.year
+            end  
+            if e.endDate.present?
+              ed << e.endDate.month 
+              ed << e.endDate.year
             end 
-        ue = user.experiences.build(company_name:e.company.name, title:e.title, is_current:e.isCurrent, description:e.summary, start_date:sd.join("-"), end_date:ed.join("-"), location:auth.extra.raw_info.location.name, user_id:user.id)
-      end
-      end
+            ue = user.experiences.build(company_name:e.company.name, title:e.title, is_current:e.isCurrent, description:e.summary, start_date:sd.join("-"), end_date:ed.join("-"), location:auth.extra.raw_info.location.name, user_id:user.id)
+          end
+        end
       
         user_educations = auth.extra.raw_info.educations.values[1]
         if auth.extra.raw_info.educations.values[1].present?          
           user_educations.each do |edu|
             ued = user.educations.build(school_name:edu.schoolName, field_of_study:edu.fieldOfStudy, start_date:edu.startDate.year, end_date:edu.endDate.year, activities:edu.activities, notes:edu.notes, degree:edu.degree)
-        end       
+          end       
         end
+        
+        #user_groups = auth.extra.raw_info.groupMemberships.values[1]
+        #if auth.extra.raw_info.groupMemberships.values[1].present?
+          #user_groups.each do |grp|
+            #ugroup = user.groups.build(name:grp.group.name, membership_state:grp.membershipState.code)
+          #end
+        #end
     end
     user
   end
@@ -198,6 +205,10 @@ private
   
   def create_user_educations
     self.educations.create
+  end
+  
+  def create_user_groups
+    self.groups.create
   end
   
   def check_provider? 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121203172252) do
+ActiveRecord::Schema.define(:version => 20121204112654) do
 
   create_table "countries", :force => true do |t|
     t.string   "name",       :default => "", :null => false
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(:version => 20121203172252) do
     t.string   "description"
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
+  end
+
+  create_table "groups", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "membership_state"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
   create_table "profiles", :force => true do |t|
